d43b396e2ab5f13eb4e295a3010f414a9a72363c
Javier Carnero <jcarnero@plainconcepts.com>
2019-10-31 16:55:07 +0100

    Better automatic loading of environments
    - A bug prevented environments to automatically register when no
    explicit import to the environment was made

--- arlie/arlie/envs/manage.py
+++ arlie/arlie/envs/manage.py
@@ -7,6 +7,16 @@ from urllib.request import urlopen
 from tempfile import NamedTemporaryFile
 
 
+def _envs_dir(base_dir):
+    envs = next(os.walk(base_dir))[1]
+    try:
+        envs.remove("__pycache__")
+    except ValueError:
+        pass
+
+    return envs
+
+
 def _download_extract_zip(url, path):
     """
     Download a ZIP file and extract its contents in path
@@ -18,14 +28,17 @@ def _download_extract_zip(url, path):
         zfile.extractall(path)
 
 
+def _load_envs(base_dir, envs):
+    module_prefix = ".".join(__name__.split(".")[:-1]) + "."
+    env_modules = [module_prefix + e for e in envs]
+    for modname in env_modules:
+        __import__(modname)
+
+
 def check_envs():
     base_dir = Path(os.path.dirname(os.path.abspath(__file__)))
 
-    envs = next(os.walk(base_dir))[1]
-    try:
-        envs.remove("__pycache__")
-    except ValueError:
-        pass
+    envs = _envs_dir(base_dir)
 
     if not envs:
         tag = "v" + __version__
@@ -40,9 +53,13 @@ def check_envs():
         _download_extract_zip(url, base_dir)
         print("..install complete.")
 
-                if platform.system() == "Linux":
+        if platform.system() == "Linux":
             import stat
 
             for name in base_dir.rglob("*.exe"):
                 st = os.stat(name)
                 os.chmod(name, st.st_mode | stat.S_IEXEC)
+
+        envs = _envs_dir(base_dir)
+
+    _load_envs(base_dir, envs)
